e38fd4589c9c077cc4bff928f7c954cad00c1f92
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-03-12 13:07:39 +0100

    book data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,18 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { InfoBoxComponent } from './info-box/info-box.component';
 import { MouseCursorComponent } from './mouse-cursor/mouse-cursor.component';
 import { TitleBoxComponent } from './title-box/title-box.component';
+import { BookDataService } from './books/book-data.service';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, InfoBoxComponent, MouseCursorComponent, TitleBoxComponent],
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    InfoBoxComponent,
+    MouseCursorComponent,
+    TitleBoxComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, BooksModule, AboutModule],
-  providers: [],
+  providers: [BookDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
