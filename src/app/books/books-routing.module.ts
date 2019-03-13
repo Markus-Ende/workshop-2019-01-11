80a90429958c7c96f05bd3784b03a3abe17a3c06
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-03-13 10:23:01 +0100

    initial routing

--- src/app/books/books-routing.module.ts
+++ src/app/books/books-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/books'
+  },
+  {
+    path: 'books',
+    component: BookListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class BooksRoutingModule { }
+export class BooksRoutingModule {}
