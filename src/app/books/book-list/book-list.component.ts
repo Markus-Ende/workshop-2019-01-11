d43e4e90c28eaaa6998b09d54ac0b359c0bb927a
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2019-03-13 08:39:48 +0100

    async pipe

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -9,7 +9,7 @@ import {
 } from '@angular/core';
 import { BookDataService } from '../book-data.service';
 import { Book } from '../book';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -17,46 +17,10 @@ import { map } from 'rxjs/operators';
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent
-  implements
-    OnInit,
-    OnDestroy,
-    DoCheck,
-    OnChanges,
-    AfterViewInit,
-    AfterViewChecked {
-  books: Book[];
-  private subscription = Subscription.EMPTY;
+export class BookListComponent {
+  readonly books$: Observable<Book[]>;
 
-  constructor(private booksService: BookDataService) {
-    console.log('constructor');
-  }
-
-  ngDoCheck() {
-    console.log('do check');
-  }
-
-  ngOnInit() {
-    console.log('BookListComponent ngOnInit');
-
-    this.subscription = this.booksService
-      .getBooks()
-      .subscribe(booksFromService => (this.books = booksFromService));
-  }
-
-  ngOnChanges() {
-    console.log('BookListComponent ngOnCHanges');
-  }
-
-  ngOnDestroy() {
-    console.log('BookListComponent ngOnDestroy');
-    this.subscription.unsubscribe();
-  }
-
-  ngAfterViewInit() {
-    console.log('BookListComponent ngAfterViewInit');
-  }
-  ngAfterViewChecked() {
-    console.log('BookListComponent ngAfterViewChecked');
+  constructor(booksService: BookDataService) {
+    this.books$ = booksService.getBooks();
   }
 }
